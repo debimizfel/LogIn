9c07bcc3c176efa359b8bd5864e8cd053195f21a
debimizfel <debimizfel@gmail.com>
2021-09-13 12:13:57 -0400

    comienzo del singup

---
+++ lib/screens/Singup/components/body.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:login/components/rounded_button.dart';
+import 'package:login/screens/LogIn/components/already_have_an_account.dart';
+import 'package:login/screens/Singup/components/background.dart';
+import 'package:login/screens/LogIn/components/rounded_input_field.dart';
+import 'package:login/screens/LogIn/components/rounded_password_field.dart';
+
+import '../../../constants.dart';
+
+class Body extends StatelessWidget {
+  const Body({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              'Sing Up',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: size.height * 0.03),
+          Image.asset(
+            'assets/images/login3.png',
+            height: size.height * 0.30,
+          ),
+          SizedBox(height: size.height * 0.03),
+          RoundedInputField(
+            color: kPrimaryColorOrange,
+            hinText: 'Your Email',
+            onChange: (value) {},
+          ),
+          RoundedPasswordField(
+            color: kPrimaryColorOrange,
+            onChange: (value) {},
+          ),
+          RoundedButton(
+            text: 'SINGUP',
+            press: () {},
+            color: kPrimaryColorOrange,
+          ),
+          SizedBox(height: size.height * 0.03),
+          AlreadyHaveAnAccount(
+            press: () => Navigator.pushNamed(context, 'singup_screen'),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/Singup/singup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:login/screens/Singup/components/body.dart';
+
+class SingUpScreen extends StatelessWidget {
+  const SingUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Body(),
+      ),
+    );
+  }
+}
