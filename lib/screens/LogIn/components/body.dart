f9a771c9850f0802ec564a444dc03058c40df800
debimizfel <debimizfel@gmail.com>
2021-09-13 12:12:32 -0400

    cajas de texto y boton en el login

--- lib/screens/LogIn/components/body.dart
+++ lib/screens/LogIn/components/body.dart
@@ -1,5 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:login/components/rounded_button.dart';
+import 'package:login/screens/LogIn/components/already_have_an_account.dart';
 import 'package:login/screens/LogIn/components/background.dart';
+import 'package:login/screens/LogIn/components/rounded_input_field.dart';
+import 'package:login/screens/LogIn/components/rounded_password_field.dart';
+// import 'package:login/screens/LogIn/components/text_field_container.dart';
+
+import '../../../constants.dart';
 
 class Body extends StatelessWidget {
   const Body({
@@ -26,6 +34,25 @@ class Body extends StatelessWidget {
             'assets/images/login2.png',
             height: size.height * 0.30,
           ),
+          SizedBox(height: size.height * 0.03),
+          RoundedInputField(
+            color: kPrimaryColorPinck,
+            hinText: 'Your Email',
+            onChange: (value) {},
+          ),
+          RoundedPasswordField(
+            color: kPrimaryColorPinck,
+            onChange: (value) {},
+          ),
+          RoundedButton(
+            text: 'LOGIN',
+            press: () {},
+            color: kPrimaryColorPinck,
+          ),
+          SizedBox(height: size.height * 0.03),
+          AlreadyHaveAnAccount(
+            press: () => Navigator.pushNamed(context, 'singup_screen'),
+          )
         ],
       ),
     );
