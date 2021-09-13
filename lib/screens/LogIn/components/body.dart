790fdb2e55ea405b8f045cb5e7271969b8032a25
debimizfel <debimizfel@gmail.com>
2021-09-13 13:11:25 -0400

    implementación del ChildeScrollView

--- lib/screens/LogIn/components/body.dart
+++ lib/screens/LogIn/components/body.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:login/components/rounded_button.dart';
-import 'package:login/screens/LogIn/components/already_have_an_account.dart';
+import 'package:login/components/rounded_password_field.dart';
 import 'package:login/screens/LogIn/components/background.dart';
 import 'package:login/screens/LogIn/components/rounded_input_field.dart';
-import 'package:login/screens/LogIn/components/rounded_password_field.dart';
-// import 'package:login/screens/LogIn/components/text_field_container.dart';
-
+import 'package:login/screens/already_have_an_account.dart';
 import '../../../constants.dart';
 
 class Body extends StatelessWidget {
@@ -18,42 +16,45 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text(
-              'LOGIN',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                'LOGIN',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          SizedBox(height: size.height * 0.03),
-          Image.asset(
-            'assets/images/login2.png',
-            height: size.height * 0.30,
-          ),
-          SizedBox(height: size.height * 0.03),
-          RoundedInputField(
-            color: kPrimaryColorPinck,
-            hinText: 'Your Email',
-            onChange: (value) {},
-          ),
-          RoundedPasswordField(
-            color: kPrimaryColorPinck,
-            onChange: (value) {},
-          ),
-          RoundedButton(
-            text: 'LOGIN',
-            press: () {},
-            color: kPrimaryColorPinck,
-          ),
-          SizedBox(height: size.height * 0.03),
-          AlreadyHaveAnAccount(
-            press: () => Navigator.pushNamed(context, 'singup_screen'),
-          )
-        ],
+            SizedBox(height: size.height * 0.03),
+            Image.asset(
+              'assets/images/login2.png',
+              height: size.height * 0.30,
+            ),
+            SizedBox(height: size.height * 0.03),
+            RoundedInputField(
+              color: kPrimaryColorPinck,
+              hinText: 'Your Email',
+              onChange: (value) {},
+            ),
+            RoundedPasswordField(
+              color: kPrimaryColorPinck,
+              onChange: (value) {},
+            ),
+            RoundedButton(
+              text: 'LOGIN',
+              press: () {},
+              color: kPrimaryColorPinck,
+            ),
+            SizedBox(height: size.height * 0.01),
+            AlreadyHaveAnAccount(
+              login: true,
+              press: () => Navigator.pushNamed(context, 'singup_screen'),
+            )
+          ],
+        ),
       ),
     );
   }
