8f3e4284bfd0f9cbdeede7ad7d52aa6717870064
debimizfel <debimizfel@gmail.com>
2021-09-13 12:11:25 -0400

    cambio de colores

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff508ec7);
+const kPrimaryColorOrange = Color(0xffeb8557);
 const kPrimaryLightColor = Color(0xfff1e6ff);
+
+const kPrimaryColorPinck = Color(0xffe5527d);
